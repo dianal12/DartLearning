8c0d6171ea1ef7e6e4033d1593414b8441972636
Diana Baibulatova <135941402+dianal12@users.noreply.github.com>
2025-07-08 16:55:51 +0900

    Create FinalVariables.dart

---
+++ FinalVariables.dart
@@ -0,0 +1,7 @@
+void main() {
+  final name = 'Diana'; // we can add type but it's optional
+  // final variables can only be set once and are
+  // immutable
+  // name = 'dana'; // can't change the value of a final variable
+  print(name);
+}
